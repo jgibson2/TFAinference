57a95de02c8502ebcd67704c6786c4da5abe4557
John Gibson <johngibson@wustl.edu>
2018-05-10 23:15:05 -0400

    Refine testConvergence, implemented convergence test (using CS criteria) for gene expr matrix

--- testConvergence.py
+++ testConvergence.py
@@ -12,20 +12,30 @@ def read_all_matricies(filename, iterations):
     return np.array([data[mat_length*i:mat_length*(i+1)] for i in range(iterations)])
 
 
-def check_cs_convergence(cs_mats, in_a_row=3):
+def check_cs_convergence(cs_mats, in_a_row=3, log_limit=0.5):
     iar = 0
     for i in range(0, len(cs_mats) - 1):
-        if csConverges(cs_mats[i], cs_mats[i+1]):
+        if csConverges(cs_mats[i], cs_mats[i+1], log_limit=log_limit):
             iar += 1
             if iar >= in_a_row:
                 return True, i+1
     return False, -1
 
 
-def check_tfa_convergence(tfa_mats, in_a_row=3):
+def check_geneexpr_convergence(mats, in_a_row=3, log_limit=0.5):
+    iar = 0
+    for i in range(0, len(mats) - 1):
+        if csConverges(mats[i], mats[i+1], log_limit=log_limit): # just use CS convergence criteria
+            iar += 1
+            if iar >= in_a_row:
+                return True, i+1
+    return False, -1
+
+
+def check_tfa_convergence(tfa_mats, in_a_row=3, log_limit=0.5):
     iar = 0
     for i in range(0, len(tfa_mats) - 1):
-        if tfaConverges(tfa_mats[i], tfa_mats[i+1]):
+        if tfaConverges(tfa_mats[i], tfa_mats[i+1], log_limit=log_limit):
             iar += 1
             if iar >= in_a_row:
                 return True, i+1
@@ -46,6 +56,7 @@ def main():
     parser.add_argument('--variances', '-v', type=str, nargs='+', help='List of variance explained files', required=True)
     parser.add_argument('--iterations', '-i', type=int, action='store', default=100, help='Number of iterations in log files')
     parser.add_argument('--breaks', '-b', type=int, action='store', default=20, help='Number of iterations to consider at a time')
+    parser.add_argument('--std_dev', action='store', type=str, help='Calculate standard deviations matrix for final iterations and write to file')
     args = parser.parse_args()
     for index, file in enumerate(args.cs):
         cs_names[index] = file
@@ -62,6 +73,8 @@ def main():
                 line = line.strip()
                 vars_tmp.append(float(line))
         varsExplained.append(vars_tmp)
+    geneexpr_matricies = [[np.dot(C,A) for C,A in zip(cs, tfa)] for cs,tfa in zip(cs_matricies, tfa_matricies)]
+    geneexpr_convergence_points = [-1 for i in range(len(geneexpr_matricies))]
 
     for i in np.arange(0, args.iterations, args.breaks):
         cs = [x[i:i+args.breaks] for x in cs_matricies]
@@ -76,9 +89,14 @@ def main():
                 tfa_converges, tfa_conv_pt = check_tfa_convergence(t)
                 if tfa_converges:
                     tfa_convergence_points[index] = tfa_conv_pt + i
+            if geneexpr_convergence_points[index] == -1:
+                geneexpr_converges, geneexpr_conv_pt = check_geneexpr_convergence(t)
+                if geneexpr_converges:
+                    geneexpr_convergence_points[index] = geneexpr_conv_pt + i
 
     cs_convergences = [[] for i in range(len(cs_matricies))]
     tfa_convergences = [[] for i in range(len(tfa_matricies))]
+    geneexpr_convergences = [[] for i in range(len(geneexpr_matricies))]
 
     for index,conv_pt in enumerate(cs_convergence_points):
         if conv_pt != -1:
@@ -93,6 +111,13 @@ def main():
                 if index2 != index:
                     if tfaConverges(tfa_matricies[index][conv_pt], tfa_matricies[index2][conv_pt2]):
                         tfa_convergences[index].append(index2)
+                        
+    for index,conv_pt in enumerate(geneexpr_convergence_points):
+        if conv_pt != -1:
+            for index2, conv_pt2 in enumerate(geneexpr_convergence_points):
+                if index2 != index:
+                    if csConverges(geneexpr_matricies[index][conv_pt], geneexpr_matricies[index2][conv_pt2]): #use CS criteria
+                        geneexpr_convergences[index].append(index2)
 
     for index, other_matricies in enumerate(cs_convergences):
         print('{0}-th matrix from {1} converged to same solution as: '.format(cs_convergence_points[index], 
@@ -106,5 +131,15 @@ def main():
         print(' '.join(
             ['{0}-th matrix from {1}'.format(tfa_convergence_points[x], tfa_names[x]) for x in other_matricies]))
     
+    for index, other_matricies in enumerate(geneexpr_convergences):
+        print('{0}-th matrix from {1} converged to same solution as: '.format(geneexpr_convergence_points[index],
+                                                                              '{0}*{1}'.format(cs_names[index], tfa_names[index]), end=''))
+        print(' '.join(
+            ['{0}-th matrix from {1}'.format(geneexpr_convergence_points[x], '{0}*{1}'.format(cs_names[index], tfa_names[index])) for x in other_matricies]))
+
+    if args.std_dev:
+        final_iter_matricies = [g[-1] for g in geneexpr_matricies]
+        writeMatrixToFile(np.nanstd(np.array(final_iter_matricies), axis=0), args.std_dev) # clever hack to form a matrix of std. devs of each parameter
+
 if __name__ == '__main__':
     main()
